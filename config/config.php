e5aba802237577f0f223b8ddd60c83c3b7c25af0
Kodmit <alexandre.bailly@live.fr>
2017-01-18 23:02:22 +0100

    Retrait de fonctions inutiles

--- config/config.php
+++ config/config.php
@@ -1,23 +1,7 @@
 <?php
-
-// Edit the values bellow :
-
-$userSystem = true;
-$useDatabase = true;
 $devMode = true;
 
 if ($devMode) {
 	error_reporting(E_ALL);
 	ini_set("display_errors", 1);
-}
-
-if($useDatabase){
-	require('././bundles/database.php');
-}
-
-
-// Don't edit the values bellow !
-
-if($userSystem){
-	include('././bundles/user.php');
 }
