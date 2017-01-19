2e7b7804e04402af52708271eb7779f948032200
Kodmit <alexandre.bailly@live.fr>
2017-01-19 14:26:40 +0100

    Update home.php

--- templates/home.php
+++ templates/home.php
@@ -1,16 +1,16 @@
 Obtenir un l'email de l'utilisateur kodmit : <?= $bundle->getClass('userBundle')->get('email', 'Kodmit'); ?>
 
-Ajouter un utilisateur : <?= $bundle->getClass('userBundle')->addUser('kodmit', 'password', 'email'); ?>
+Ajouter un utilisateur : <?= //$bundle->getClass('userBundle')->addUser('kodmit', 'password', 'email'); ?>
 
 Verifier si une info utilisateur existe : 
 <?php
-if($bundle->getClass('userBundle')->userExist('email', 'test@test.fr')){
-  echo "l'utlisateur avec cet email existe";
-}
+//if($bundle->getClass('userBundle')->userExist('email', 'test@test.fr')){
+//  echo "l'utlisateur avec cet email existe";
+//}
 ?>
 
 Mettre un utilisateur à jours :
-<?php $bundle->getClass('userBundle')->updateUser('kodmit', 'email', 'new_email@test.com'); ?>
+<?php //$bundle->getClass('userBundle')->updateUser('kodmit', 'email', 'new_email@test.com'); ?>
 
 Supprimer un utilisateur :
-<?php $bundle->getClass('userBundle')->removeUser('kodmit'); ?>
+<?php //$bundle->getClass('userBundle')->removeUser('kodmit'); ?>
