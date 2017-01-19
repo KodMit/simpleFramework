0ca09f1d9843b0a088033d160a49c120797e978a
Kodmit <alexandre.bailly@live.fr>
2017-01-19 14:50:47 +0100

    Adaptation du système de routage

--- index.php
+++ index.php
@@ -4,21 +4,20 @@ require('./config/config.php');
 require('./routing.php');
 require('./bundles.php');
 
-if (!isset($_GET['page'])) {
-	header("Location: index.php?page=");
-}
-
-// Bundle system
 $bundle = new Bundle();
 foreach ($bundle->getBundles() as $bundleM) {
 	include('./bundles/'.$bundleM->file);
 	$bundleM->class = new $bundleM->class;
 }
 
-// Routing system
 $routing = new Routing();
 foreach ($routing->getRoutes() as $data) {
-	if ($_GET['page'] == $data->link) {
-		include('templates/'.$data->file);
+	if (isset($_GET['page'])) {
+		if ($_GET['page'] == $data->link) {
+			include('templates/'.$data->file);
+		}
+	}
+	else{
+		include_once('templates/'.$routing->getFile("index"));
 	}
 }
